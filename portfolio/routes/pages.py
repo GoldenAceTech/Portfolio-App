0b45121078dfe8ae6a9905b35d9253d98d72f09f
Your Name <goldenacetech@hotmail.com>
2022-06-05 23:59:14 -0500

    added and styled the project and contact pages

--- portfolio/routes/pages.py
+++ portfolio/routes/pages.py
@@ -3,9 +3,36 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('pages', __name__)
 
+projects = [
+    {
+        "name": "Vacation space rental app with NodeJs and MongoDB",
+        "thumb": "img/vacation-rental.jpg",
+        "hero": "img/vacation-rental-hero.jpg",
+        "categories": ["NodeJs", "Javascript"],
+        "slug": "vacation-rental",
+        "prod": "https://modern-stay.herokuapp.com/",
+    },
+    {
+        "name": "Habit Tracking app with Python (Flask) and MongoDB",
+        "thumb": "img/habit-tracker.jpg",
+        "hero": "img/habit-tracker-hero.jpg",
+        "categories": ["Python", "Flask", "Web"],
+        "slug": "habit-tracker",
+        "prod": "https://ace-habit-tracker-app.herokuapp.com/",
+    },
+    {
+        "name": "Microblog app with Python (Flask) and MongoDB",
+        "thumb": "img/microblog.jpg",
+        "hero": "img/microblog-hero.jpg",
+        "categories": ["Python", "Flask", "Web"],
+        "slug": "microblog",
+        "prod": "https://ace-microblog-app.herokuapp.com/",
+    },
+]
+
 @bp.route('/')
 def home():
-    return render_template('pages/index.html', title='Home')
+    return render_template('pages/index.html', title='Home', projects=projects)
 
 @bp.route('/about')
 def about():
--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,14 +1,23 @@
 from flask.testing import FlaskClient
 
+
 def test_home(client: FlaskClient):
-    response = client.get('/')
+    response = client.get("/")
     assert b"Home | Portfolio" in response.data
+    assert b"Vacation space rental app with NodeJs and MongoDB" in response.data
+    assert b"Habit Tracking app with Python (Flask) and MongoDB" in response.data
+    assert b"Microblog app with Python (Flask) and MongoDB" in response.data
+
 
 def test_about(client: FlaskClient):
-    response = client.get('/about')
+    response = client.get("/about")
     assert b"About | Portfolio" in response.data
     assert b"Hi, I'm Afolabi!" in response.data
 
+
 def test_contact(client: FlaskClient):
-    response = client.get('/contact')
+    response = client.get("/contact")
     assert b"Contact | Portfolio" in response.data
+    assert b"Email:" in response.data
+    assert b"GitHub:" in response.data
+    assert b"Twitter:" in response.data
